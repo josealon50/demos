a8c98cf3271033afd03a54dc1acb9c8e2a249bce
Jose Alonso Leon <jleon@sandiegobloodbank.org>
2020-04-14 17:25:21 -0700

    Added namespace to each models

--- demos/app/Models/Stores/Stores.php
+++ demos/app/Models/Stores/Stores.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Stores;
 
 use Illuminate\Database\Eloquent\Model;
 
--- demos/app/Models/Vendors/Vendors.php
+++ demos/app/Models/Vendors/Vendors.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Vendors;
 
 use Illuminate\Database\Eloquent\Model;
 
